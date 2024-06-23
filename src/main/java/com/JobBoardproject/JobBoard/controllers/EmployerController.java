fad77643b1c1741da3b8dae8001306bb951d964f
Coder_Sushil <sushilkumarrohidas0@gmail.com>
2024-06-23 16:17:06 +0530

    Starting with Employer Dashboad Job posting form reintegration in the Employer Dashboardis done

--- src/main/java/com/JobBoardproject/JobBoard/controllers/EmployerController.java
+++ src/main/java/com/JobBoardproject/JobBoard/controllers/EmployerController.java
@@ -1,2 +1,16 @@
-package com.JobBoardproject.JobBoard.controllers;public class EmployerController {
+package com.JobBoardproject.JobBoard.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Employer")
+public class EmployerController {
+
+    @GetMapping("/profile")
+    public String EmpDashboard(Model model) {
+        return "Employer_Dashboard";
+    }
 }
